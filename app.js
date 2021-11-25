a3d64fefbd33ee0e56e0721f5dfae6ad26a41368
GodfreySam <godfreysam09@gmail.com>
2021-11-25 09:36:15 +0100

    fixes

--- app.js
+++ app.js
@@ -11,14 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "client/build")));
-
-	app.get("*", function (req, res) {
-		res.sendFile(path.join(__dirname, "client/build", "index.html"));
-	});
-}
-
 routers(app);
 
 app.use(logger("dev"));
